dcaa7d60f6a78dfe8047821b45e598201ca1f81b
marie taylor <marietaylortailor@hotmail.com>
2023-05-05 12:49:57 +0100

    test updated with new function layout and arguements reflected in test suite

--- spellChecker.test.js
+++ spellChecker.test.js
@@ -1,10 +1,8 @@
 const spellChecker = require("./spellChecker");
 
-// test("should throw error if input not a string", () => {
-//   expect(() => spellChecker("sentence must be a string")).toThrowError(
-//     "sentence must be a string"
-//   );
-// });
+test("should throw error if input not a string", () => {
+  expect(() => spellChecker(123)).toThrowError("sentence must be a string");
+});
 
 test("should throw error if input sentence is empty", () => {
   expect(() => spellChecker("")).toThrowError("sentence cannot be empty");
